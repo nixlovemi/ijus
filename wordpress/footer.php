de956a790ea8cf89465388e5c69f104813ac0866
nixlovemi <nixlovemi@gmail.com>
2017-11-16 11:04:50 -0200

    Alterei a index para dar GET no header/footer (wordpress)

---
+++ wordpress/footer.php
@@ -0,0 +1,95 @@
+    <aside class="bg-primary-dark footer">
+            <div class="container text-justify text-white">
+                <div class="row">
+                    <div class="col-lg-3 col-lg-offset-2">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <img src="<?php bloginfo('template_url'); ?>/img/logotipo.png" alt="">
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-lg-10">
+                                <p style="font-size: 12px;">Acompanhe e participe<br> das ações do IJUS em Juruti. </p>
+                                <p style="font-size: 12px;line-height: .5;margin-bottom:10px;">Acesse nosso Facebook</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <a href="https://www.facebook.com/ijus.org.br/" class="text-primary white-hover fa fa-2x fa-facebook-square"></a>&nbsp;
+
+                                <a href="tel:55 +93 99190-0791" class="text-primary white-hover"><i class="fa fa-2x fa-whatsapp"></i>&nbsp;+55 93 99190-0791</a>&nbsp;
+
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-12">
+                                Contato: contato@ijus.org.br
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-offset-1 col-lg-2 border-col">
+                        <br><br>
+                        <a href="sobre-nos.php" class="text-white">Quem Somos?</a> <br>
+                        <a href="index.php#noticias" class="text-white">Notícias</a> <br>
+                        <a href="downloads.php" class="text-white">Editais & TDRs</a> <br>
+                        <a href="transparencia.php" class="text-white">Portal da Transparência</a> <br>
+                        <a href="imprensa.php" class="text-white">Sala da imprensa</a> <br>
+                        <a href="boletim-informativo.php" class="text-white">Boletim Informativo</a> <br>
+                        <a href="agenda.php" class="text-white">Agenda</a> <br>
+                        <a href="contato.php" class="text-white">Contato</a> <br>
+                        <a href="investidores.php" class="text-white">Seja um investidor</a> <br>
+                    </div>
+
+                    <div class="col-lg-2">
+                        <br><br>
+                        <div class="text-primary-dark">
+                            NOTICIAS
+                        </div>
+                        <a href="views/noticias/05-04/noticia.php" class="text-white font-footer">Alterações</a><br>
+                        <a href="views/noticias/01-07/noticia.php" class="text-white font-footer">IJUS lança edital</a><br>
+                        <a href="views/noticias/15-07/noticia.php" class="text-white font-footer">IJUS apoia produção</a><br>
+                        <a href="views/noticias/01-06/noticia.php" class="text-white font-footer">IJUS incentiva esportes</a><br>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </aside>
+    <style media="screen">
+        .font-footer{
+            font-size: 15px;
+        }
+    </style>
+
+    <!-- jQuery -->
+    <script src="<?php bloginfo('template_url'); ?>/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php bloginfo('template_url'); ?>/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="<?php bloginfo('template_url'); ?>/js/creative.js"></script>
+    <script type="text/javascript">
+        $('[data-slide="prev-news"]').click(function () {
+            $('#myCarousel-news').carousel('prev');
+            $('#myCarousel-news').carousel('pause');
+        });
+        $('[data-slide="next-news"]').click(function () {
+            $('#myCarousel-news').carousel('next');
+            $('#myCarousel-news').carousel('pause');
+        });
+    </script>
+
+    <?php wp_footer(); ?>
+</body>
+
+</html>
